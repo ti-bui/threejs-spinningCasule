1e41efd66ff39292c60f390e2a759f83ef02ca08
ti-bui <buimydung2001@gmail.com>
2023-10-15 19:16:56 -0400

    basic scene

--- src/script.js
+++ src/script.js
@@ -1,31 +1,37 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+// SCENE
+const scene = new THREE.Scene();
+// CANVAS
+const canvas = document.querySelector("canvas.webgl");
 
-// Scene
-const scene = new THREE.Scene()
+// GEOMETRY
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
-// Sizes
+//SIZE
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+//CAMERA
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 3;
+scene.add(camera);
 
-// Renderer
+//RENDERER
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+//ANIMATION
